a1bfe98a2e6b282872b9bf71088f75d47f57cb7c
fambirachimwe <36159141+Fambirachimwe@users.noreply.github.com>
2023-08-28 11:09:34 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # clicknpayMobilePayments
+
+## Introduction
+
+clicknpaymobile is a lightweight library that simplifies the integration of ClicknPay payment gateway with your Node.js web applications .Clicknpay OPEN API is a popular payment processor that allows merchants to accept ecocash and onemoney payments online securely and conveniently.
+
+## Prerequisites
+
+In order to make use of this project, the following prerequisites must be met for it to work.
+
+1. Node version 0.6.0 and above
+2. NPM (node's package manager, used to install the node library)
+3. Axios (make sure axios is install because it is a dependency)
+
+## Example code
+
+```js
+const Clicknpay = require("clicknpaymobile");
+const payment = new clicknpay("Unique code from the dashboard");
+
+// initiate a mobile payement via USSD tot the customer
+payment
+  .mobilePayment("<Amount>", "<customer Phone number>", "Currency code")
+  .then((data) => {
+    // handle the response
+  });
+```
+
+### Check transaction status
+
+```js
+payment
+  .mobilePayment("<Amount>", "<customer Phone number>", "Currency code")
+  .then((data) => {
+    // handle the response
+    payment.chechTransactionStatus(data).then((data) => {
+      console.log(data);
+    });
+  });
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+const axios = require('axios');
+
+function Clicknpaymoblie(uniqueId) {
+    this.uniqueId = uniqueId;
+
+    this.mobilePayment = async (amount, customerPhoneNumber, currencyCode) => {
+        const ecocashUrl = 'https://backendservices.clicknpay.africa:2081/ecocashapi/initiate';
+        const netoneUrl = 'https://backendservices.clicknpay.africa:2081/onemoneyapi/initiate';
+        const econet = /^07[78]/; // regex for econet phone number
+        const netone = /^071/; // regex for netone phone number
+        let method;
+        let response;
+
+        if (econet.test(customerPhoneNumber)) {
+            method = 'ecocash';
+        } else if (netone.test(customerPhoneNumber)) {
+            method = 'onemoney';
+        }
+
+        try {
+            const data = {
+                amount: amount,
+                currency: currencyCode,
+                endUserId: customerPhoneNumber,
+                uniqueId: this.uniqueId,
+                description: 'Mobile payment',
+            };
+
+            if (method === 'ecocash') {
+                response = await axios.post(ecocashUrl, data);
+            } else if (method === 'onemoney') {
+                response = await axios.post(netoneUrl, data);
+            }
+            return response?.data;
+        } catch (error) {
+            console.log(error);
+            return error
+        }
+
+    }
+
+
+    // use the resonse from the mobile payment function
+    this.chechTransactionStatus = async (res) => {
+        const ecocashUrl = `https://backendservices.clicknpay.africa:2081/ecocashapi/status/${res.enduserId}/${res.clientCorrelator}`;
+        const netoneUrl = `https://backendservices.clicknpay.africa:2081/onemoneyapi/status/${res.endUserId}/${res.clientCorrelator}`;
+
+        const econet = /^7[78]/; // regex for econet phone number
+        const netone = /^71/; // regex for netone phone number
+
+        let method;
+        let response;
+
+        if (econet.test(res.endUserId)) {
+            method = 'ecocash';
+        } else if (netone.test(res.endUserId)) {
+            method = 'onemoney';
+        }
+
+        if (method === 'ecocash') {
+            response = await axios.get(ecocashUrl);
+        } else if (method === 'onemoney') {
+            response = await axios.post(netoneUrl);
+        }
+
+        return response?.data;
+
+
+    }
+}
+
+module.exports = Clicknpaymoblie
